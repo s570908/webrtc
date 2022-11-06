bef055805f06dd07939364f4a04fa1503ac33013
s570908 <songohyoung@gmail.com>
2022-11-06 15:52:05 +0900

    약간의 코멘트와 webRTC 참고 자료를 추가하였다.

--- server.js
+++ server.js
@@ -1,81 +1,83 @@
+const express = require("express");
 
-const express = require('express')
+var io = require("socket.io")({
+  path: "/webrtc",
+});
 
-var io = require('socket.io')
-({
-  path: '/webrtc'
-})
-
-const app = express()
-const port = 8080
+const app = express();
+const port = 8080;
 
 //app.get('/', (req, res) => res.send('Hello World!!!!!'))
 
 //https://expressjs.com/en/guide/writing-middleware.html
-app.use(express.static(__dirname + '/build'))
-app.get('/', (req, res, next) => {
-    res.sendFile(__dirname + '/build/index.html')
-})
+app.use(express.static(__dirname + "/build"));
+app.get("/", (req, res, next) => {
+  res.sendFile(__dirname + "/build/index.html");
+});
 
-const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
-io.listen(server)
+io.listen(server);
 
 // https://www.tutorialspoint.com/socket.io/socket.io_namespaces.htm
-const peers = io.of('/webrtcPeer')
+const peers = io.of("/webrtcPeer");
 
 // keep a reference of all socket connections
-let connectedPeers = new Map()
+let connectedPeers = new Map();
 
-peers.on('connection', socket => {
-
+peers.on("connection", (socket) => {
   // convenience function to log server messages on the client
   function log() {
-    var array = ['Message from server:'];
+    var array = ["Message from server:"];
     array.push.apply(array, arguments);
-    socket.emit('log', array);
+    socket.emit("log", array);
   }
 
   log(`Server created socket with socketID: ${socket.id}`);
   console.log(`Server created socket with socketID: ${socket.id}`);
 
-  socket.emit('connection-success', { success: socket.id })
+  socket.emit("connection-success", { success: socket.id });
 
-  connectedPeers.set(socket.id, socket)
+  connectedPeers.set(socket.id, socket);
 
-  socket.on('disconnect', () => {
-    log(`${socket.id} disconnected`)
-    console.log(`${socket.id} disconnected`)
-    connectedPeers.delete(socket.id)
-  })
+  socket.on("disconnect", () => {
+    log(`${socket.id} disconnected`);
+    console.log(`${socket.id} disconnected`);
+    connectedPeers.delete(socket.id);
+  });
 
-  socket.on('offerOrAnswer', (data) => {
-    log(`${socket.id} said: offerOrAnswer`)
-    console.log(`${socket.id} said: offerOrAnswer`)
+  socket.on("offerOrAnswer", (data) => {
+    log(`${socket.id} said: offerOrAnswer`);
+    console.log(`${socket.id} said: offerOrAnswer`);
     // send to the other peer(s) if any
     for (const [socketID, socket] of connectedPeers.entries()) {
       // don't send to self
       if (socketID !== data.socketID) {
-        log(`Signaling Server sent offerOrAnswer to ${socketID} with data.payload.type: ${data.payload.type}`)
-        console.log(`Signaling Server sent offerOrAnswer to ${socketID} with data.payload.type: ${data.payload.type}`)
-        socket.emit('offerOrAnswer', data.payload)
+        log(
+          `Signaling Server sent offerOrAnswer to ${socketID} with data.payload.type: ${data.payload.type}`
+        );
+        console.log(
+          `Signaling Server sent offerOrAnswer to ${socketID} with data.payload.type: ${data.payload.type}`
+        );
+        socket.emit("offerOrAnswer", data.payload);
       }
     }
-  })
+  });
 
-  socket.on('candidate', (data) => {
-    log(`${socket.id} said: candidate`)
-    console.log(`${socket.id} said: candidate`)
+  socket.on("candidate", (data) => {
+    log(`${socket.id} said: candidate`);
+    console.log(`${socket.id} said: candidate`);
 
     // send candidate to the other peer(s) if any
     for (const [socketID, socket] of connectedPeers.entries()) {
       // don't send to self
       if (socketID !== data.socketID) {
-        log(`Signaling Server sent candidate to ${socketID} with data.payload: ${data.payload}`)
-        console.log(`Signaling Server sent candidate to ${socketID} with data.payload: ${data.payload}`)
-        socket.emit('candidate', data.payload)
+        log(`Signaling Server sent candidate to ${socketID} with data.payload: ${data.payload}`);
+        console.log(
+          `Signaling Server sent candidate to ${socketID} with data.payload: ${data.payload}`
+        );
+        socket.emit("candidate", data.payload);
       }
     }
-  })
-
-})
+  });
+});
